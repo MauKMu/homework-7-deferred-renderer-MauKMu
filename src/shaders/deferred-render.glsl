b5966cec277d74791929140b3a511627f8f548d8
MauKMu <maukmu@users.noreply.github.com>
2018-03-24 13:46:29 -0400

    Add Lambert shading w/ fixed light

--- src/shaders/deferred-render.glsl
+++ src/shaders/deferred-render.glsl
@@ -20,6 +20,13 @@ uniform vec4 u_CamPos;
 const float CAMERA_TAN = tan(0.5 * 45.0 * 3.1415962 / 180.0);
 const float DEPTH_OFFSET = 0.125;
 
+const vec3 LIGHT_POS = vec3(0, 20, 10);
+
+float getLambert(vec3 worldPos, vec3 normal) {
+    vec3 toLight = normalize(LIGHT_POS - worldPos);
+    return clamp(0.0, 1.0, dot(toLight, normal));
+}
+
 void main() { 
 	// read from GBuffers
 	vec4 gb0 = texture(u_gb0, fs_UV);
@@ -41,20 +48,15 @@ void main() {
     // shade
     else {
         depth += DEPTH_OFFSET;
-        col = albedo;
+        //col = albedo;
         // get cam-space position
         vec3 ndcPos = vec3(fs_UV.xy * 2.0 - 1.0, depth);
-        //vec3 ndcPos = vec3(fs_UV.x * 2.0 - 1.0, 1.0 - fs_UV.y * 2.0, depth);
-        //col = abs(ndcPos - vec3(gb0.xy / 760.0, gb0.z)) * 0.1;
-        //col = abs(ndcPos - vec3(gb0.x * 2.0 - 1.0, 1.0 - gb0.y, gb0.z)) * 1.0;
         float vert = CAMERA_TAN * abs(depth);
         float hori = vert * u_AspectRatio;
         vec3 camPos = ndcPos * vec3(hori, vert, 1.0);
-        //col = abs(camPos - gb0.xyz) * 1.0;
-        //col = col.yyy;
-        vec3 worPos = vec3(inverse(u_View) * vec4(camPos, 1.0));
-        //col = abs(worPos - gb0.xyz) * 1.0;
-        //col = vec3(depth - gb0.z);
+        // convert to world-space pos
+        vec3 worldPos = vec3(inverse(u_View) * vec4(camPos, 1.0));
+        col = (0.2 + 0.8 * getLambert(worldPos, nor)) * albedo;
     }
 
 	out_Col = vec4(col, 1.0);
